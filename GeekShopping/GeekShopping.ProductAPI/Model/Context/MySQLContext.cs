52000a0aaaaeb7e6188968de78d3d2d980dbc675
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-11-05 15:49:40 -0300

    Creating entity product and add to database context

--- GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -11,5 +11,7 @@ namespace GeekShopping.ProductAPI.Model.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
         {
         }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
