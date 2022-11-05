824866f1d462931f631e164aadcf08d79cc6bb2f
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-11-05 17:45:40 -0300

    Creating MappingConfig and config AutoMapper in program.cs

--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,14 +1,18 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
 var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+var mapper = MappingConfig.RegisterMaps().CreateMapper();
 
 builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 31))));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShoppingApi", Version = "v1" });
