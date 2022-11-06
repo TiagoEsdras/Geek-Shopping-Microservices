2b60757cddfb1c104173eedb354ffdfa9cb33c54
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-11-06 14:13:36 -0300

    Creating HttpClientExtensions

---
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GeekShopping.Web.Utils;
+
+public static class HttpClientExtensions
+{
+    private static readonly MediaTypeHeaderValue contentType = new("application/json");
+
+    public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode)
+            throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+
+        var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+        var t = JsonSerializer.Deserialize<T>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                if (t is null)
+            throw new ArgumentNullException();
+
+        return t;
+    }
+
+    public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T t)
+    {
+        var content = new StringContent(JsonSerializer.Serialize(t));
+
+        content.Headers.ContentType = contentType;
+
+        return httpClient.PostAsync(url, content);
+    }
+
+    public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T t)
+    {
+        var content = new StringContent(JsonSerializer.Serialize(t));
+
+        content.Headers.ContentType = contentType;
+
+        return httpClient.PutAsync(url, content);
+    }
+}
