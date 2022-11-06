68ef25fb3d19a8edc01252d9bc02d8c92267f9d2
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-11-06 13:38:30 -0300

    Creating IProductService

---
+++ GeekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductModel>> FindAll();
+
+    Task<ProductModel> FindById(long id);
+
+    Task<ProductModel> Create(ProductModel productModel);
+
+    Task<ProductModel> Update(ProductModel productModel);
+
+    Task<bool> DeleteById(long id);
+}
