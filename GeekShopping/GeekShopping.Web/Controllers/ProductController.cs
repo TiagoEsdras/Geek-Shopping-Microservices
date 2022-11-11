56dc9a559d68942f10fe17e1c8cb5cf86d4250b9
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-11-10 21:17:45 -0300

    adjusments on layout.cshtml and creating productController and ProductIndex

---
+++ GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            var products = await productService.FindAll();
+            return View(products);
+        }
+    }
+}
