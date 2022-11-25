2b948a1b591fe6d3d90c52dcd215338aca67bb82
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-11-25 11:37:46 -0300

    implementing methods to add and remove coupon on cart

--- GeekShopping/GeekShopping.Web/Services/IServices/ICartService.cs
+++ GeekShopping/GeekShopping.Web/Services/IServices/ICartService.cs
@@ -12,7 +12,7 @@ public interface ICartService
 
     Task<bool> RemoveFromCart(long cartId, string token);
 
-    Task<bool> ApplyCoupon(CartViewModel cart, string couponCode, string token);
+    Task<bool> ApplyCoupon(CartViewModel cart, string token);
 
     Task<bool> RemoveCoupon(string userId, string token);
 
